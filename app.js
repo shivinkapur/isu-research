894b15f16092d83812597e38fad5ea08aba6dd17
Song Zheng <song@zheng.club>
2018-03-14 16:43:56 -0700

    working prototypew

--- app.js
+++ app.js
@@ -26,6 +26,16 @@ const post = (url, data, cb) => {
   });
 };
 
+// Helper function to send post request and error handling
+const post2 = (url, data, cb) => {
+  request.post(url, data, (err, res, body) => {
+    if (err) 
+      return console.log('error', err);
+    const result = typeof body === 'string' ? JSON.parse(body) : body;
+    cb( result );
+  });
+};
+
 // When app starts, get an oauth token
 post('/oauth2/token', {
   form: {
@@ -88,30 +98,85 @@ app.post('/merchant/referrals', (req,res) => {
             "partner_client_id": cid,
             "feature_list": [
               'ADVANCED_TRANSACTIONS_SEARCH',
+              'PAYMENT',
+              'REFUND',
+              'PARTNER_FEE',
             ]
           }
         },
       }],
       web_experience_preference: {
         partner_logo_url: 'https://pbs.twimg.com/profile_images/960328536606191616/M_8xKwuC_400x400.jpg',
-        return_url: 'https://ppm.songz.me/merchant/return'
-      }
+        return_url: 'https://ppm.songz.me/merchant/return',
+        return_url_description: 'RETURN TO AWESOME SAUCE',
+        use_mini_browser: true
+      },
     }
   }, (body) => {
     res.json(body);
   });
 });
 
+app.get('/merchant/balances', (req, res) => {
+  // Get merchant information (like target cid)
+  request.get(`${ppUrl}/payment-experience/web-profiles?client_id=AZLu80lpVPlRrDMfNb5rXrPqb-EacAvi1PRiOAE4X9FQ7W8BSK8qinEvSf0aGNz6qEIc464R1NRJYpii`, {
+    headers: {
+      Authorization: `Bearer ${ppdata.access_token}`
+    },
+  }, (err, rez, body) => {
+    if(err) return res.json(err);
+    if(!body) return res.json(rez);
+    const result = (typeof body === 'string') ? JSON.parse(body) : body;
+    res.json(result);
+  });
+});
+
 // User is returned here after ISU flow
 app.get('/merchant/return', (req,res) => {
   // Get merchant information (like target cid)
+  const queries = req.query;
   request.get(`${ppUrl}/customer/partners/${partnerId}/merchant-integrations/${req.query.merchantIdInPayPal}`, {
     headers: {
       Authorization: `Bearer ${ppdata.access_token}`
     },
   }, (err, rez, body) => {
     const result = (typeof body === 'string') ? JSON.parse(body) : body;
-    res.json(result);
+
+    // When app starts, get an oauth token
+    post('/oauth2/token', {
+      form: {
+        grant_type: 'client_credentials' ,
+        target_client_id: result.oauth_integrations[0].oauth_third_party[0].merchant_client_id,
+      },
+      auth: {
+        user: cid,
+        pass: secret,
+      },
+    }, (body) => {
+      result.newData = body;
+      result.queries = req.query;
+
+
+      //request.get(`${ppUrl}/offers/containers`, {
+      //request.get(`${ppUrl}/identity/openidconnect/userinfo?schema=openid`, {
+      //request.get(`${ppUrl}/identity/oauth2/userinfo?schema=openidconnect`, {
+      console.log(`${ppUrl}/customer/partners/merchant-accounts/${result.merchant_id}`);
+      request.get(`${ppUrl}/customer/partners/merchant-accounts/${result.merchant_id}`, {
+        headers: {
+          Authorization: `Bearer ${body.access_token}`
+        },
+      }, (err2, rez2, body2) => {
+        if(err2) return res.json(err2);
+        if(!body2) return res.json(rez2);
+        const result3 = (typeof body2 === 'string') ? JSON.parse(body2) : body2;
+        result.newData = result3;
+        res.json(result);
+      });
+
+
+      //res.json(result);
+    });
+
   });
 });
 
